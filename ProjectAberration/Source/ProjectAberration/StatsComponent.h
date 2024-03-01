9aceaa229792bc0e47490c6df228166b5c2843eb
Teodor Mihăescu <teodor@mihaescu.info>
2024-03-01 23:01:03 +0100

    FEAT: Project configuration and concept WIP

---
+++ ProjectAberration/Source/ProjectAberration/StatsComponent.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "StatsComponent.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class PROJECTABERRATION_API UStatsComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UStatsComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+private:
+	int health = 0;
+	int attack = 0;
+	float earthAttackMultiplier = 0.0f;
+	float fireAttackMultiplier = 0.0f;
+	float waterAttackMultiplier = 0.0f;
+	float airAttackMultiplier = 0.0f;
+	float earthDefenseMultiplier = 0.0f;
+	float fireDefenseMultiplier = 0.0f;
+	float waterDefenseMultiplier = 0.0f;
+	float airDefenseMultiplier = 0.0f;
+
+public:
+};
