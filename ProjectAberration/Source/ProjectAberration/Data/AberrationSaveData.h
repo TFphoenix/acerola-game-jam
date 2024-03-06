ae6e560e83abb9eb6c882010c67b0f295f7f2c4c
Teodor Mihăescu <teodor@mihaescu.info>
2024-03-07 00:51:54 +0100

    Implement Save Data persistance

---
+++ ProjectAberration/Source/ProjectAberration/Data/AberrationSaveData.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BodyPartDataAsset.h"
+#include "AberrationSaveData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FRegionProgressData
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 RegionId = 1;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsLocked;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsConquered;
+};
+
+USTRUCT(BlueprintType)
+struct FAberrationPlayerData
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Name;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<UBodyPartDataAsset*> AvailableBodyParts;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<UBodyPartDataAsset*> CurrentBodyParts;
+};
